7cdf2f07df4962eb2aa7d8bef2180e73126273e6
SJH <252355189@qq.com>
2018-07-23 22:28:06 +0800

    添加Zuul过滤器

--- ApiGateway/src/main/java/com/hdu/sjh/apigateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/hdu/sjh/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.hdu.sjh.apigateway;
 
+import com.hdu.sjh.apigateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -11,4 +13,9 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+	@Bean
+    public AccessFilter accessFilter() {
+	    return new AccessFilter();
+    }
 }
